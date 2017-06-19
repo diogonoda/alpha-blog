5308847b8ba9a0da87f85d82fca1000803c3d53b
Diogo Tsuguio Noda-DNODA <diogo.noda@walmart.com>
2017-06-19 08:31:46 -0300

    Model validations

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20170616205903) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
