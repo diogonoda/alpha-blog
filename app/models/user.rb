3683b3031b3c8885755b0be2beaca900df208106
Diogo Tsuguio Noda-DNODA <diogo.noda@walmart.com>
2017-06-21 09:55:56 -0300

    relations added and byebug tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
+  has_many :articles
+  has_many :comments
+
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true, uniqueness: { case_sensivite: false },
                        length: { minimum: 3, maximum: 50 }
 
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEXP }
-
-  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616205903) do
+ActiveRecord::Schema.define(version: 20170621120548) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
