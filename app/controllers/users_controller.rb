d79a6e4dfadf0cae42c67db0042f7a1234ef225d
Diogo Noda <diogonoda@hotmail.com>
2017-07-13 00:13:56 -0300

    Added user edition feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+#  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -19,6 +19,19 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfuly"
+
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   # POST /users
@@ -36,17 +49,17 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#  def update
+#    respond_to do |format|
+#      if @user.update(user_params)
+#        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+#        format.json { render :show, status: :ok, location: @user }
+#      else
+#        format.html { render :edit }
+#        format.json { render json: @user.errors, status: :unprocessable_entity }
+#      end
+#    end
+#  end
 
   # DELETE /users/1
   # DELETE /users/1.json
@@ -60,9 +73,9 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+#    def set_user
+#      @user = User.find(params[:id])
+#    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
