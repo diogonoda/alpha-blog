535d7cfde0ab5b557ca36e7a408455f309dc783b
Diogo Noda <diogonoda@hotmail.com>
2017-07-19 00:06:39 -0300

    Controlling sessions for login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
   
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully login"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "User or email invalid"
+
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You're logged out"
+
+    redirect_to root_path
   end
 end
