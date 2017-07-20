3eadff6272a6a8216272d969f133e821a42e8fda
Diogo Noda <diogonoda@hotmail.com>
2017-07-19 23:35:08 -0300

    Authentication controllers added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,23 @@
 class UsersController < ApplicationController
-#  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 4)
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfuly"
 
@@ -36,8 +27,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -49,8 +38,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
@@ -60,12 +47,10 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-#    def set_user
-#      @user = User.find(params[:id])
-#    end
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
