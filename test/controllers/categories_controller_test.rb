7f12f7943051b32548f4b70be77bdb963ead27d4
Diogo Tsuguio Noda-DNODA <diogo.noda@walmart.com>
2017-10-25 11:18:01 -0200

    Restricting access to new categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "diogo", email: "email@email.com", password: "passwd", admin: true)
   end
 
   test "should get categories index" do
@@ -11,13 +12,23 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    session[:user_id] = @user.id
+
     get :new
     assert_response :success
   end
 
   test "should get show" do
-#    get(:show, {'id' => @category.id})
     get :show, params: { id: @category.id }
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+#      post :create, category: { name: "sports" }
+      post :create, params: { category: { name: "sports"} }
+    end
+
+    assert_redirected_to categories_path
   end
 end
