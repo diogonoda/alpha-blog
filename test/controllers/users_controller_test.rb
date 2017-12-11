49e0f826541cc7cd1980f1b2d12c3e69bbc8bb28
Diogo Tsuguio Noda-DNODA <diogo.noda@walmart.com>
2017-12-11 11:13:17 -0200

    Last commit

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,48 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-#  setup do
-#    @user = users(:one)
-#  end
-#
-#  test "should get index" do
-#    get users_url
-#    assert_response :success
-#  end
-#
-#  test "should get new" do
-#    get new_user_url
-#    assert_response :success
-#  end
-#
-#  test "should create user" do
-#    assert_difference('User.count') do
-#      post users_url, params: { user: { email: @user.email, string: @user.string, username: @user.username } }
-#    end
-#
-#    assert_redirected_to user_url(User.last)
-#  end
-#
-#  test "should show user" do
-#    get user_url(@user)
-#    assert_response :success
-#  end
-#
-#  test "should get edit" do
-#    get edit_user_url(@user)
-#    assert_response :success
-#  end
-#
-#  test "should update user" do
-#    patch user_url(@user), params: { user: { email: @user.email, string: @user.string, username: @user.username } }
-#    assert_redirected_to user_url(@user)
-#  end
-#
-#  test "should destroy user" do
-#    assert_difference('User.count', -1) do
-#      delete user_url(@user)
-#    end
-#
-#    assert_redirected_to users_url
-#  end
+  test "should successfully login" do
+    get signup_path
+
+    assert_template 'users/new'
+
+    assert_difference 'User.count', 1 do
+      post users_path, params:{
+                               user:{
+                                     username: "Test User",
+                                     email: "user@test.com",
+                                     password: "passwd",
+                                     admin: true
+                                     }
+                               }
+
+      follow_redirect!
+    end
+
+    assert_template 'users/show'
+
+    assert_match "Test User", response.body
+  end
 end
